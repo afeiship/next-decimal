0b27c4808bc0472b1c571c54752a1ccb6068b1b2
afeiship <1290657123@qq.com>
2017-03-03 23:22:52 +0800

    otpimize

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ GH_REPO="@github.com/afeiship/next-decimal.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
 ## commit to github:
+npm run build
 git config user.name "afeiship-ci"
 git config user.email "1290657123@qq.com"
 git add .
