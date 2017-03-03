a45d97ddd7309879b926b700f584bbe3d56c6fa3
afeiship <1290657123@qq.com>
2017-03-03 23:22:14 +0800

    update deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## basic define:
+GH_REPO="@github.com/afeiship/next-decimal.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+## commit to github:
+git config user.name "afeiship-ci"
+git config user.email "1290657123@qq.com"
+git add .
+git commit -m "deployed to github by travis-ci"
+git push --force --quiet $FULL_REPO master:master
