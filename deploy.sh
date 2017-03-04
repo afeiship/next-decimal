259ff1239c30540a1da10ca1ca97d0002942ba16
afeiship <1290657123@qq.com>
2017-03-04 21:14:49 +0800

    updte test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## basic define:
+REPO_TOKEN="4a6932953b7a065834aff8cb82a66dcab7d6ee8f"
 GH_REPO="github.com/afeiship/next-decimal.git"
 FULL_REPO="https://$REPO_TOKEN@$GH_REPO"
 
@@ -8,6 +9,7 @@ echo "$FULL_REPO";
 
 
 ## commit to github:
+git config --global push.default simple
 git config user.name "feizheng-ci"
 git config user.email "1290657123@qq.com"
 git status
@@ -16,5 +18,5 @@ git add .
 git commit -m "deployed to github by travis-ci"
 
 echo "Start to push ...."
-git push
+git push --force $FULL_REPO master:master
 echo "End to push ci..."
