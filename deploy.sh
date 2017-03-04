981632b710659f6949c27f7995b2cceb2d271b6b
afeiship <1290657123@qq.com>
2017-03-04 11:20:01 +0800

    test ci build3

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 ## basic define:
-GH_REPO="@github.com/afeiship/next-decimal.git"
-FULL_REPO="https://$GH_TOKEN$GH_REPO"
+GH_REPO="github.com/afeiship/next-decimal.git"
+FULL_REPO="https://$GH_TOKEN@$GH_REPO"
 
 echo "$FULL_REPO";
 
 
 ## commit to github:
 cd ~/build/afeiship/next-decimal
+ls -alh
 npm run build
 git config user.name "afeiship"
 git config user.email "1290657123@qq.com"
-git add .
-git commit -m "deployed to github by travis-ci"
-git push --force $FULL_REPO master:master
+git add --all
+git commit -m "Deployed to github by travis-ci"
+git push --force "$FULL_REPO" master:master
 
